d31c5212176075071cfc5bd2bb14cde6ac5c8f23
MitjaHaimila <mitja.haimila@gmail.com>
2020-07-10 10:22:28 +0300

    lisätty kommentit tehtävä kakkoseen

--- vko2/tehtava2.py
+++ vko2/tehtava2.py
@@ -1,16 +1,20 @@
 import argparse
 import boto3
 
+# argparserin setup, lisätty integer-tyyppinen argumentti "rivit"
 parser = argparse.ArgumentParser()
 parser.add_argument("rivit", help="tulostettavien rivien lukumäärä", type=int)
 args = parser.parse_args()
 
+# tiedoston lataaminen bucketista
 s3 = boto3.client("s3")
 s3.download_file("mitja-checkpoint-vko2","checkpoint1.txt","checkpoint1.txt")
 
+# avaa ladatun tiedoston ja lisää uuteen listaan rivit nollasta komentoriviargumentin lukuun asti
 with open('checkpoint1.txt') as tekstitiedosto:
     nimilista = tekstitiedosto.readlines()[0:args.rivit]
 
+# yllä tehdyn listan sorttaus list itemin pituuden perusteella ja listan yhdistäminen stringiksi
 nimilista.sort(key=len)
 ordered_list = "".join(nimilista)
 
